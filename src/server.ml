6e9bf2c66fbd5439a3426fa67906674853b16eb7
Sacha-Élie Ayoun <sachaayoun@gmail.com>
2025-01-10 15:36:19 +0000

    support lsp 1.19 & 1.20
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>

--- src/server.ml
+++ src/server.ml
@@ -568,7 +568,17 @@ module Make (IO : IO) = struct
           | Lsp.Client_request.CallHierarchyOutgoingCalls _
           | Lsp.Client_request.WillCreateFiles _
           | Lsp.Client_request.WillDeleteFiles _
-          | Lsp.Client_request.WillRenameFiles _ ->
+          | Lsp.Client_request.WillRenameFiles _
+          | Lsp.Client_request.InlayHintResolve _
+          | Lsp.Client_request.TextDocumentDiagnostic _
+          | Lsp.Client_request.TextDocumentInlineCompletion _
+          | Lsp.Client_request.TextDocumentInlineValue _
+          | Lsp.Client_request.TextDocumentPrepareTypeHierarchy _
+          | Lsp.Client_request.TextDocumentRangesFormatting _
+          | Lsp.Client_request.WorkspaceSymbolResolve _
+          | Lsp.Client_request.WorkspaceDiagnostic _
+          | Lsp.Client_request.TypeHierarchySubtypes _
+          | Lsp.Client_request.TypeHierarchySupertypes _ ->
             let notify_back =
               new notify_back
                 ~workDoneToken:None ~partialResultToken:None ~notify_back
@@ -754,7 +764,11 @@ module Make (IO : IO) = struct
         | Lsp.Client_notification.DidCreateFiles _
         | Lsp.Client_notification.DidDeleteFiles _
         | Lsp.Client_notification.WorkDoneProgress _
-        | Lsp.Client_notification.DidRenameFiles _ ->
+        | Lsp.Client_notification.DidRenameFiles _
+        | Lsp.Client_notification.NotebookDocumentDidOpen _
+        | Lsp.Client_notification.NotebookDocumentDidChange _
+        | Lsp.Client_notification.NotebookDocumentDidClose _
+        | Lsp.Client_notification.NotebookDocumentDidSave _ ->
           let notify_back =
             new notify_back
               ~workDoneToken:None ~partialResultToken:None ~notify_back
